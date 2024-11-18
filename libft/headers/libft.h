dde7d1bf5bce0a41016b7b02e0af53417eb30fe5
Tobias Keil <tkeil@3-C-10.42heilbronn.de>
2024-11-18 19:21:23 +0100

    updated libft.h

--- libft/headers/libft.h
+++ libft/headers/libft.h
@@ -17,6 +17,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include "../srcs/get_next_line/get_next_line.h"
+# include "../srcs/get_next_line/get_next_line_bonus.h"
 
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
