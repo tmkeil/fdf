bda9f0011587b0a47672c628661bec9475979610
Tobias Keil <tkeil@2-I-3.42heilbronn.de>
2024-11-19 21:52:41 +0100

    up fdf.h

--- headers/fdf.h
+++ headers/fdf.h
@@ -6,7 +6,7 @@
 /*   By: tkeil <tkeil@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 01:59:27 by tkeil             #+#    #+#             */
-/*   Updated: 2024/11/19 19:18:21 by tkeil            ###   ########.fr       */
+/*   Updated: 2024/11/19 21:37:33 by tkeil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -78,9 +78,17 @@ typedef struct s_wireframe
 	t_line			*lines;
 }					t_wireframe;
 
+// typedef struct s_map
+// {
+// 	t_point			**matrix;
+// 	int				*widths;
+// 	int				height;
+// }					t_map;
+
 typedef struct s_map
 {
-	t_point			**matrix;
+	int				**profile;
+	int				**colors;
 	int				*widths;
 	int				height;
 }					t_map;
@@ -89,15 +97,29 @@ typedef struct s_map
 int					ft_init_imgs(t_data **data);
 int					ft_init_var(t_data **data);
 int					ft_init(t_data **data);
-void				ft_set_current(t_data *data, t_image_data **current);
 
 //
-void				del(void *content);
 int					ft_abs(int value);
 void				ft_clear(t_data *data);
 int					ft_rgb(int r, int g, int b);
 void				ft_arrangeline(t_line *line);
 void				ft_putpxl(t_image_data *img, int x, int y, int color);
 void				ft_paint_buffer(t_mlx *mlx, t_image_data *img, int color);
+
+// utils
+int					ft_map_height(int fd);
+void				ft_set_current(t_data *data, t_image_data **current);
+
+// utils2
+size_t				ft_len(char **map_ln);
+int					ft_abs(int value);
+int					ft_rgb(int r, int g, int b);
+int					ft_wordcount(char *line);
+
+// numbers
+long				ft_atol(char *s);
+
+// parse
+int					ft_parsemap(t_map **map, char **argv);
 
 #endif
