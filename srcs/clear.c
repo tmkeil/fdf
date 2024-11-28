bf54ee4f223c75df8b58488c14415457bbff6182
Tobias Keil <tkeil@student.42heilbronn.de>
2024-11-28 04:01:57 +0100

    reupdated the clear again

--- srcs/clear.c
+++ srcs/clear.c
@@ -6,7 +6,7 @@
 /*   By: tkeil <tkeil@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/20 13:13:32 by tkeil             #+#    #+#             */
-/*   Updated: 2024/11/28 03:46:10 by tkeil            ###   ########.fr       */
+/*   Updated: 2024/11/28 04:01:11 by tkeil            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,21 +57,26 @@ void	ft_clear_wireframe(t_wire *wirefr)
 	}
 }
 
-void	ft_clear_mlx(t_mlx *var)
+void	ft_clear_mlx(t_data *data)
 {
-	if (var)
-	{
-		if (var->mlx_win)
-			mlx_destroy_window(var->mlx_ptr, var->mlx_win);
-		free(var);
-	}
+    if (data && data->var)
+    {
+        if (data->img && data->img->current && data->img->current->img)
+        {
+            mlx_destroy_image(data->var->mlx_ptr, data->img->current->img);
+            data->img->current->img = NULL;
+        }
+        if (data->var->mlx_win)
+            mlx_destroy_window(data->var->mlx_ptr, data->var->mlx_win);
+        free(data->var);
+    }
 }
 
 void	ft_cleardata(t_data *data)
 {
 	if (data)
 	{
-		ft_clear_mlx(data->var);
+		ft_clear_mlx(data);
 		ft_clear_images(data->img);
 		ft_clear_wireframe(data->wirefr);
 		free(data);
